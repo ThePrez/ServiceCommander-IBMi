22479e35a869dcf0ca773006315c75d98a5a1317
Jesse Gorzinski <jgorzinski@gmail.com>
2022-04-01 18:44:22 -0400

    cleanup unnecessary batch logic

--- strtcpsvr/sc_tcpsvr.c
+++ strtcpsvr/sc_tcpsvr.c
@@ -270,8 +270,6 @@ int main(int argc, char *argv[])
     child_argv[1] = "/QOpenSys/pkgs/bin/bash";
     child_argv[2] = "-l";
     child_argv[3] = "-c";
-    int is_it_batch = is_batch();
-    char* batch_prefix_string = (0 == is_it_batch) ? "exec" : "";
     char sc_cmd[1024];
     snprintf(sc_cmd, sizeof(sc_cmd), "exec /QOpenSys/pkgs/bin/sc -a %s %s %s 2>&1", sc_options, sc_operation, instance);
     child_argv[4] = sc_cmd;
@@ -281,7 +279,7 @@ int main(int argc, char *argv[])
     char *envp[10];
     envp[0] = "QIBM_MULTI_THREADED=Y";
     envp[1] = "PATH=/QOpenSys/pkgs/bin:/QOpenSys/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin";
-    envp[2] = is_it_batch? "QIBM_USE_DESCRIPTOR_STDIO=N" : "QIBM_USE_DESCRIPTOR_STDIO=Y";
+    envp[2] = "QIBM_USE_DESCRIPTOR_STDIO=Y";
     envp[3] = "PASE_STDIO_ISATTY=N";
     struct passwd *pd;
     char logname[20];
@@ -331,13 +329,6 @@ int main(int argc, char *argv[])
     }
     // We don't need to talk to the child's stdin, so let's close it.
     close(stdoutFds[1]);
-    if(is_it_batch)
-    {
-        Qp0zLprintf("Submitted command: '%s'\n", sc_cmd);
-        Qp0zLprintf("Check spooled file output for progress\n");
-        close(stdoutFds[0]);
-        return parm->rc = 255;
-    }
 
     // Now, let's read the output from the child process and print it here.
     char line[1024 * 4];
